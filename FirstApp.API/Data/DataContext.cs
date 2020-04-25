4f6c9a57eef826a03075ef2f59b08a2552293987
Korczyn666 <szymon89098@gmail.com>
2020-04-25 18:05:16 +0200

    Initial Commit

---
+++ FirstApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using FirstApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
